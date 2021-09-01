0fb478a1e355967f030509e39015337791ff5f7f
modesty0310 <rlatprua4@gmail.com>
2021-09-01 13:28:02 +0900

    할일 추가 기능

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const List = require('../models/list')
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.render('index', {message: 'hello'})
+  List.find()
+  .then((list) => {res.render('index', {message: `오늘 할일 ${list.length}`, todolist: list})
+console.log(list)})
+  .catch((err) => console.log(err));
 });
 
+router.post('/', (req,res,next) => {
+  const list = {title : req.body.content};
+  List.create(list)
+  .then(()=>res.redirect('/'))
+  .catch((err) => console.log(err))
+})
+
 module.exports = router;
+
